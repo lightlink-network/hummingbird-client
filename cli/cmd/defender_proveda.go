b935182ee0ccd5c22ba07acf163311e71975a8d8
KassCrypto <kasscrypto@gmail.com>
2024-01-08 09:56:56 +0000

    added: defender proveda command

---
+++ cli/cmd/defender_proveda.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"hummingbird/config"
+	"hummingbird/defender"
+	"hummingbird/node"
+	"hummingbird/utils"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	DefenderProveDaCmd.PersistentFlags().String("tx", "", "celestia tx hash in which data was submitted")
+	DefenderProveDaCmd.Flags().Bool("json", false, "output proof in json format")
+}
+
+var DefenderProveDaCmd = &cobra.Command{
+	Use:   "prove-da",
+	Short: "prove-da will prove a data availability batch",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.Load()
+		logger := DefaultLogger()
+		ethKey := getEthKey()
+
+		n, err := node.NewFromConfig(cfg, logger, ethKey)
+		utils.NoErr(err)
+
+		d := defender.NewDefender(n, &defender.Opts{
+			Logger: logger.With("ctx", "Defender"),
+		})
+
+		txHash := common.HexToHash(cmd.Flag("tx").Value.String())
+		proof, err := d.ProveDA(txHash)
+		if err != nil {
+			logger.Error("Failed to prove data availability", "err", err)
+			panic(err)
+		}
+
+		if cmd.Flag("json").Value.String() == "true" {
+			buf, err := json.MarshalIndent(proof, "", "  ")
+			utils.NoErr(err)
+			fmt.Println(string(buf))
+			return
+		}
+
+		wrappedProof, err := rlp.EncodeToBytes(proof.WrappedProof)
+		utils.NoErr(err)
+
+		fmt.Println("Proof:")
+		fmt.Println("	Nonce:", proof.Nonce)
+		fmt.Println("	Tuple.Height:", proof.Tuple.Height)
+		fmt.Println("	Tuple.DataRoot:", common.Hash(proof.Tuple.DataRoot).Hex())
+		fmt.Println("	WrappedProof:", hexutil.Encode(wrappedProof))
+	},
+}

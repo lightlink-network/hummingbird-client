3eafe573541a57a1394ea42daa58952918944f8b
KassCrypto <kasscrypto@gmail.com>
2024-01-08 10:01:02 +0000

    register cmds

--- cli/main.go
+++ cli/main.go
@@ -7,12 +7,32 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hummingbird",
-	Short: "Hummingbird is LightLinks rollup node",
+	Use:   "hb",
+	Short: "Hummingbird is LightLinks rollup node. It can be used to create new rollup blocks, download state, create and respond to challenges, and more.",
+}
+
+var rollupCmd = &cobra.Command{
+	Use:   "rollup",
+	Short: "rollup is a command to interact with the LL rollups",
+}
+
+var defenderCmd = &cobra.Command{
+	Use:   "defender",
+	Short: "defender is a command to generate proofs and respond to challenges",
 }
 
 func main() {
-	rootCmd.AddCommand(cmd.RollupCmd)
+	// add subcommands to defender
+	defenderCmd.AddCommand(cmd.DefenderProveDaCmd)
+
+	// add subcommands to rollup
+	rollupCmd.AddCommand(cmd.RollupInfoCmd)
+	rollupCmd.AddCommand(cmd.RollupNextCmd)
+
+	// add all commands to root
+	rootCmd.AddCommand(rollupCmd)
+	rootCmd.AddCommand(defenderCmd)
+
 	err := rootCmd.Execute()
 	if err != nil {
 		panic(err)

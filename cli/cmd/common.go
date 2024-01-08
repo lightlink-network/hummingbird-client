13edb946dd8b926c948a2a3c2981a8c1e7231adb
KassCrypto <kasscrypto@gmail.com>
2024-01-08 09:56:00 +0000

    moved config to seperate pkg

--- cli/cmd/common.go
+++ cli/cmd/common.go
@@ -7,50 +7,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/spf13/viper"
 )
-
-type Config struct {
-	StorePath string `mapstructure:"storePath"`
-	Celestia  struct {
-		Token         string `mapstructure:"token"`
-		Endpoint      string `mapstructure:"endpoint"`
-		Namespace     string `mapstructure:"namespace"`
-		TendermintRPC string `mapstructure:"tendermint_rpc"`
-		GRPC          string `mapstructure:"grpc"`
-	} `mapstructure:"celestia"`
-	Ethereum struct {
-		Endpoint            string `mapstructure:"endpoint"`
-		CanonicalStateChain string `mapstructure:"canonicalStateChain"`
-	} `mapstructure:"ethereum"`
-	LightLink struct {
-		Endpoint string `mapstructure:"endpoint"`
-		Delay    int    `mapstructure:"delay"`
-	} `mapstructure:"lightlink"`
-	Rollup struct {
-		PollDelay             int    `mapstructure:"pollDelay"`
-		BundleSize            uint64 `mapstructure:"bundleSize"`
-		StoreCelestiaPointers bool   `mapstructure:"storeCelestiaPointers"`
-		StoreHeaders          bool   `mapstructure:"storeHeaders"`
-	} `mapstructure:"rollup"`
-}
-
-func LoadConfig() *Config {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-
-	cfg := &Config{}
-	err = viper.Unmarshal(cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	return cfg
-}
 
 func DefaultLogger() *slog.Logger {
 	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	StorePath string `mapstructure:"storePath"`
+	Celestia  struct {
+		Token         string `mapstructure:"token"`
+		Endpoint      string `mapstructure:"endpoint"`
+		Namespace     string `mapstructure:"namespace"`
+		TendermintRPC string `mapstructure:"tendermint_rpc"`
+		GRPC          string `mapstructure:"grpc"`
+	} `mapstructure:"celestia"`
+	Ethereum struct {
+		Endpoint            string `mapstructure:"endpoint"`
+		CanonicalStateChain string `mapstructure:"canonicalStateChain"`
+	} `mapstructure:"ethereum"`
+	LightLink struct {
+		Endpoint string `mapstructure:"endpoint"`
+		Delay    int    `mapstructure:"delay"`
+	} `mapstructure:"lightlink"`
+	Rollup struct {
+		PollDelay             int    `mapstructure:"pollDelay"`
+		BundleSize            uint64 `mapstructure:"bundleSize"`
+		StoreCelestiaPointers bool   `mapstructure:"storeCelestiaPointers"`
+		StoreHeaders          bool   `mapstructure:"storeHeaders"`
+	} `mapstructure:"rollup"`
+}
+
+func Load() *Config {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := &Config{}
+	err = viper.Unmarshal(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg
+}

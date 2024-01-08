b0c88d335ef90706ddba69d926049bf411f576b9
KassCrypto <kasscrypto@gmail.com>
2024-01-08 23:48:58 +0000

    remove: unused defender method

--- defender/defender.go
+++ defender/defender.go
@@ -1,7 +1,6 @@
 package defender
 
 import (
-	"encoding/json"
 	"hummingbird/node"
 	"log/slog"
 
@@ -23,20 +22,4 @@ func NewDefender(node *node.Node, opts *Opts) *Defender {
 
 func (d *Defender) ProveDA(txHash common.Hash) (*node.CelestiaProof, error) {
 	return d.Celestia.GetProof(txHash[:])
-}
-
-func (d *Defender) ProveDAByBlockHash(blockHash common.Hash) (*node.CelestiaProof, error) {
-	key := append([]byte("da_pointer"), blockHash[:]...)
-	buf, err := d.Node.Store.Get(key)
-	if err != nil {
-		return nil, err
-	}
-
-	var pointer node.CelestiaPointer
-	err = json.Unmarshal(buf, &pointer)
-	if err != nil {
-		return nil, err
-	}
-
-	return d.ProveDA(pointer.TxHash)
 }

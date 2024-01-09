df64e7e19fad7b827814db467879d2f55de9295e
KassCrypto <kasscrypto@gmail.com>
2024-01-09 01:40:21 +0000

    added: new pretty Text marshaller

--- cli/cmd/rollup_info.go
+++ cli/cmd/rollup_info.go
@@ -8,7 +8,6 @@ import (
 	"hummingbird/utils"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 )
 
@@ -34,23 +33,27 @@ var RollupInfoCmd = &cobra.Command{
 		info, err := r.GetInfo()
 		utils.NoErr(err)
 
+		// fmt.Println(" ")
+		// fmt.Println("Rollup Height:", info.RollupHeight)
+		// fmt.Println("L2 Blocks Rolled Up:", info.L2BlocksRolledUp)
+		// fmt.Println("L2 Blocks Todo:", info.L2BlocksTodo)
+		// fmt.Println(" ")
+		// fmt.Println("Latest Rollup Block:")
+		// fmt.Println("	Hash:", info.LatestRollup.Hash.Hex())
+		// fmt.Println("	Bundle Size:", info.LatestRollup.BundleSize)
+		// fmt.Println("	Epoch:", info.LatestRollup.Epoch)
+		// fmt.Println("	Height:", info.LatestRollup.L2Height)
+		// fmt.Println("	Prev Hash:", common.Hash(info.LatestRollup.PrevHash).Hex())
+		// fmt.Println("	StateRoot:", common.Hash(info.LatestRollup.StateRoot).Hex())
+		// fmt.Println("	BlockRoot:", common.Hash(info.LatestRollup.BlockRoot).Hex())
+		// fmt.Println("	TxRoot:", common.Hash(info.LatestRollup.TxRoot).Hex())
+		// fmt.Println("	Data Availability:")
+		// fmt.Println("		Celestia Height:", info.LatestRollup.CelestiaHeight)
+		// fmt.Println("		Celestia Data Root:", common.Hash(info.LatestRollup.CelestiaDataRoot).Hex())
+		// fmt.Println(" ")
+
 		fmt.Println(" ")
-		fmt.Println("Rollup Height:", info.RollupHeight)
-		fmt.Println("L2 Blocks Rolled Up:", info.L2BlocksRolledUp)
-		fmt.Println("L2 Blocks Todo:", info.L2BlocksTodo)
-		fmt.Println(" ")
-		fmt.Println("Latest Rollup Block:")
-		fmt.Println("	Hash:", info.LatestRollup.Hash.Hex())
-		fmt.Println("	Bundle Size:", info.LatestRollup.BundleSize)
-		fmt.Println("	Epoch:", info.LatestRollup.Epoch)
-		fmt.Println("	Height:", info.LatestRollup.L2Height)
-		fmt.Println("	Prev Hash:", common.Hash(info.LatestRollup.PrevHash).Hex())
-		fmt.Println("	StateRoot:", common.Hash(info.LatestRollup.StateRoot).Hex())
-		fmt.Println("	BlockRoot:", common.Hash(info.LatestRollup.BlockRoot).Hex())
-		fmt.Println("	TxRoot:", common.Hash(info.LatestRollup.TxRoot).Hex())
-		fmt.Println("	Data Availability:")
-		fmt.Println("		Celestia Height:", info.LatestRollup.CelestiaHeight)
-		fmt.Println("		Celestia Data Root:", common.Hash(info.LatestRollup.CelestiaDataRoot).Hex())
+		fmt.Println(utils.MarshalText(info))
 		fmt.Println(" ")
 	},
 }
--- utils/misc.go
+++ utils/misc.go
@@ -1,7 +1,95 @@
 package utils
 
+import (
+	"encoding/hex"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 func NoErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func IndentText(text, indent string) string {
+	return strings.ReplaceAll(text, "\n", "\n"+indent)
+}
+
+func MarshalTextIndex(v interface{}, indentChar string) string {
+	return formatValueText(reflect.ValueOf(v), 0, indentChar)
+}
+
+func MarshalText(v interface{}) string {
+	return MarshalTextIndex(v, "	")
+}
+
+func formatValueText(v reflect.Value, indent int, indentChar string) string {
+	var sb strings.Builder
+
+	switch v.Kind() {
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			field := v.Type().Field(i)
+			fieldValue := v.Field(i)
+
+			sb.WriteString(strings.Repeat(indentChar, indent))
+			sb.WriteString(field.Name)
+			sb.WriteString(": ")
+
+			if fieldValue.Kind() == reflect.Struct || (fieldValue.Kind() == reflect.Ptr && !fieldValue.IsNil() && fieldValue.Elem().Kind() == reflect.Struct) {
+				sb.WriteString("\n")
+				sb.WriteString(formatValueText(fieldValue, indent+1, indentChar)) // Increment indent for nested structs
+			} else {
+				sb.WriteString(fmt.Sprintf("%v\n", formatIndividualValue(fieldValue)))
+			}
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			sb.WriteString("nil\n")
+		} else {
+			sb.WriteString(formatValueText(v.Elem(), indent, indentChar))
+		}
+
+	case reflect.Slice:
+		// if bytes array, print as hex or e, print as hex
+		if v.Type().Elem().Kind() == reflect.Uint8 {
+			sb.WriteString(fmt.Sprintf("%v\n", formatIndividualValue(v)))
+		} else {
+			for i := 0; i < v.Len(); i++ {
+				sb.WriteString(formatValueText(v.Index(i), indent, indentChar))
+			}
+		}
+
+	case reflect.Array:
+		// if bytes array, print as hex or e, print as hex
+		if v.Type().Elem().Kind() == reflect.Uint8 {
+			sb.WriteString(fmt.Sprintf("%v\n", formatIndividualValue(v)))
+		} else {
+			for i := 0; i < v.Len(); i++ {
+				sb.WriteString(formatValueText(v.Index(i), indent, indentChar))
+			}
+		}
+
+	default:
+		sb.WriteString(fmt.Sprintf("%v\n", v.Interface()))
+	}
+
+	return sb.String()
+}
+
+func formatIndividualValue(v reflect.Value) interface{} {
+	if v.Kind() == reflect.Slice && v.Type().Elem().Kind() == reflect.Uint8 { // Check for []byte
+		bytes := v.Bytes()
+		return "0x" + hex.EncodeToString(bytes)
+	}
+
+	// handle byte arrays
+	if v.Kind() == reflect.Array && v.Type().Elem().Kind() == reflect.Uint8 { // Check for [32]byte
+		bytes := v.Slice(0, v.Len()).Bytes()
+		return "0x" + hex.EncodeToString(bytes)
+	}
+
+	return v.Interface()
 }

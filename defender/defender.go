827de2fe6fb1d7ff52c35cd2ecaecbc54c7a474b
KassCrypto <kasscrypto@gmail.com>
2024-01-10 17:25:47 +0000

    Add DefenderDA method to Defender struct

--- defender/defender.go
+++ defender/defender.go
@@ -1,14 +1,17 @@
 package defender
 
 import (
+	"fmt"
 	"hummingbird/node"
 	"log/slog"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type Opts struct {
 	Logger *slog.Logger
+	DryRun bool // DryRun indicates whether or not to actually submit the block to the L1 rollup contract.
 }
 
 type Defender struct {
@@ -22,4 +25,13 @@ func NewDefender(node *node.Node, opts *Opts) *Defender {
 
 func (d *Defender) ProveDA(txHash common.Hash) (*node.CelestiaProof, error) {
 	return d.Celestia.GetProof(txHash[:])
+}
+
+func (d *Defender) DefenderDA(block common.Hash, txHash common.Hash) (*types.Transaction, error) {
+	proof, err := d.ProveDA(txHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prove data availability: %w", err)
+	}
+
+	return d.Ethereum.DefendDataRootInclusion(block, proof)
 }

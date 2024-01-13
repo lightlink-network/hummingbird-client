77033aef87cc024a3d031774045716c9cf31b6e5
Sledro <dan@sledro.com>
2024-01-13 19:13:25 +0000

    watch challenge contract and defend DA challenges

---
+++ cli/cmd/defender_start.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"hummingbird/config"
+	"hummingbird/defender"
+	"hummingbird/node"
+	"hummingbird/utils"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var DefenderStartCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start will start the defender node",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.Load()
+		logger := GetLogger(viper.GetString("log-type"))
+		ethKey := getEthKey()
+
+		n, err := node.NewFromConfig(cfg, logger, ethKey)
+		utils.NoErr(err)
+
+		d := defender.NewDefender(n, &defender.Opts{
+			Logger: logger.With("ctx", "Defender"),
+		})
+
+		for {
+			err = d.Start()
+			if err != nil {
+				logger.Error("Defender.Start failed", "err", err, "retry_in", "5s")
+			}
+			time.Sleep(5 * time.Second)
+		}
+
+	},
+}
--- defender/defender.go
+++ defender/defender.go
@@ -3,6 +3,7 @@ package defender
 import (
 	"fmt"
 	"hummingbird/node"
+	"hummingbird/node/contracts"
 	"log/slog"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -36,4 +37,47 @@ func (d *Defender) DefendDA(block common.Hash, txHash common.Hash) (*types.Trans
 
 	d.Opts.Logger.Debug("Submitting data availability proof to L1 rollup contract", "block", block.Hex(), "dataroot", hexutil.Encode(proof.Tuple.DataRoot[:]))
 	return d.Ethereum.DefendDataRootInclusion(block, proof)
+}
+
+func (d *Defender) Start() error {
+	challenges := make(chan *contracts.ChallengeContractChallengeDAUpdate)
+	subscription, err := d.Ethereum.WatchChallengesDA(challenges)
+	if err != nil {
+		return fmt.Errorf("error starting WatchChallengesDA: %w", err)
+	}
+	defer subscription.Unsubscribe()
+
+	d.Opts.Logger.Info("Defender started and watching for challenges")
+
+	for challenge := range challenges {
+		if challenge.Status != 1 {
+			continue
+		}
+
+		blockHash := common.BytesToHash(challenge.BlockHash[:])
+
+		d.Opts.Logger.Info("DA challenge received", "block", blockHash.Hex(), "block_index", challenge.BlockIndex, "expiry", challenge.Expiry, "status", challenge.Status)
+
+		celestiaTx, err := d.Store.GetDAPointer(challenge.BlockHash)
+		if err != nil {
+			d.Opts.Logger.Error("error getting CelestiaTx:", err)
+			continue
+		}
+
+		if celestiaTx == nil {
+			d.Opts.Logger.Info("no CelestiaTx found for block:", blockHash.Hex())
+			continue
+		}
+
+		d.Opts.Logger.Info("Found CelestiaTx", "tx_hash", celestiaTx.TxHash.Hex(), "block_hash", blockHash.Hex())
+
+		tx, err := d.DefendDA(challenge.BlockHash, celestiaTx.TxHash)
+		if err != nil {
+			d.Opts.Logger.Error("error defending DA:", err)
+		}
+
+		d.Opts.Logger.Info("DA challenge defended", "tx", tx.Hash().Hex(), "block", blockHash.Hex(), "block_index", challenge.BlockIndex, "expiry", challenge.Expiry, "status", challenge.Status)
+	}
+
+	return nil
 }

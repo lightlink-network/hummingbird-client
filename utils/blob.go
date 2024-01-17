0dc46204f380d083a35afbe6c7a4e0ff7b14a414
KassCrypto <kasscrypto@gmail.com>
2024-01-17 15:20:17 +0000

    Add utility functions for converting between Blob and Share types

---
+++ utils/blob.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"github.com/celestiaorg/celestia-app/pkg/appconsts"
+	"github.com/celestiaorg/celestia-app/pkg/shares"
+	"github.com/celestiaorg/celestia-node/blob"
+	"github.com/celestiaorg/celestia-node/share"
+	coretypes "github.com/tendermint/tendermint/types"
+)
+
+func BytesToBlob(ns string, buf []byte) (*blob.Blob, error) {
+
+	// get the namespace
+	_ns, err := share.NewBlobNamespaceV0([]byte(ns))
+	if err != nil {
+		return nil, err
+	}
+
+	return blob.NewBlob(0, _ns, buf)
+}
+
+func BlobToShares(b *blob.Blob) ([]shares.Share, error) {
+	_b := coretypes.Blob{
+		NamespaceVersion: b.Namespace().Version(),
+		NamespaceID:      b.NamespaceId,
+		Data:             b.Data,
+		ShareVersion:     uint8(b.ShareVersion),
+	}
+
+	return shares.SplitBlobs(_b)
+}
+
+// ShareDataStart returns the index of the first byte of the shares raw data.
+// It is after the namespace, share info byte, sequence number (if present).
+func ShareDataStart(s shares.Share) int {
+	isStart, _ := s.IsSequenceStart()
+	isCompact, _ := s.IsCompactShare()
+
+	index := appconsts.NamespaceSize + appconsts.ShareInfoBytes
+	if isStart {
+		index += appconsts.SequenceLenBytes
+	}
+	if isCompact {
+		index += appconsts.CompactShareReservedBytes
+	}
+	return index
+}
+
+// RawIndexToSharesIndex converts a raw index to a shares index, and a share index.
+// Taking into account the namespace, share info byte, sequence number (if present).
+func RawIndexToSharesIndex(rawIndex int, s []shares.Share) (share int, shareIndex int) {
+	dataRead := 0
+
+	for i := 0; i < len(s); i++ {
+		rawData, _ := s[i].RawData()
+		if rawIndex < dataRead+len(rawData) {
+			share = i
+			shareIndex = rawIndex - dataRead
+			return
+		}
+		dataRead += len(rawData)
+	}
+
+	return
+
+	// rawCursor := 0 // keeps a track of the amount of share data we have read
+	// for i := 0; i < len(s); i++ {
+	// 	shareDataSize := len(s[i].ToBytes()[ShareDataStart(s[i]):])
+	// 	if rawIndex < rawCursor+shareDataSize {
+	// 		shareIndex = rawIndex - rawCursor
+	// 		fmt.Printf("# rawIndex: %d, rawCursor: %d, shareDataSize: %d\n", rawIndex, rawCursor, shareDataSize)
+	// 		fmt.Printf("# shareIndex: %d\n", shareIndex)
+	// 		return i, shareIndex
+	// 	}
+	// 	fmt.Printf("> rawIndex: %d, rawCursor: %d, shareDataSize: %d\n", rawIndex, rawCursor, shareDataSize)
+	// 	rawCursor += shareDataSize
+	// }
+	// return
+}

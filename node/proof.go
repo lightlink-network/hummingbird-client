f3f9e8a545fa25f545c0b1067927095506f9097a
KassCrypto <kasscrypto@gmail.com>
2024-01-23 08:06:16 +0000

    implement GetShares method

--- node/proof.go
+++ node/proof.go
@@ -4,32 +4,33 @@ import (
 	"context"
 
 	"github.com/celestiaorg/celestia-app/pkg/square"
+	"github.com/celestiaorg/celestia-node/share"
 	"github.com/tendermint/tendermint/types"
 )
 
 func (c *CelestiaClient) GetShareProofs(txHash []byte, pointer *SharePointer) (*types.ShareProof, error) {
 	ctx := context.Background()
 
-	// Get the tx
+	// 1. Get the tx
 	tx, err := c.trpc.Tx(ctx, txHash, true)
 	if err != nil {
 		return nil, err
 	}
 
-	// Get the block that contains the tx
+	// 2. Get the block that contains the tx
 	blockRes, err := c.trpc.Block(context.Background(), &tx.Height)
 	if err != nil {
 		return nil, err
 	}
 
-	// Get the tx share range inside the block
-	shareRange, err := square.TxShareRange(blockRes.Block.Data.Txs.ToSliceOfBytes(), int(tx.Index), blockRes.Block.Header.Version.App)
+	// 4. Get the share range inside the block
+	shareRange, err := square.BlobShareRange(blockRes.Block.Data.Txs.ToSliceOfBytes(), int(tx.Index), 0, blockRes.Block.Header.Version.App)
 	if err != nil {
 		return nil, err
 	}
 
-	shareStart := uint64(shareRange.Start) + uint64(pointer.StartShare)
-	shareEnd := uint64(shareRange.Start) + uint64(pointer.EndShare)
+	shareStart := uint64(shareRange.Start) //+ uint64(pointer.StartShare)
+	shareEnd := uint64(shareRange.End)     //+ uint64(pointer.EndShare()+1)
 
 	// Get the shares proof
 	sharesProofs, err := c.trpc.ProveShares(ctx, uint64(tx.Height), shareStart, shareEnd)
@@ -43,4 +44,42 @@ func (c *CelestiaClient) GetShareProofs(txHash []byte, pointer *SharePointer) (*
 	}
 
 	return &sharesProofs, nil
+}
+
+func (c *CelestiaClient) GetShares(txHash []byte, namespace string) (share.NamespacedShares, error) {
+	ctx := context.Background()
+
+	ns, err := share.NewBlobNamespaceV0([]byte(c.Namespace()))
+	if err != nil {
+		return nil, err
+	}
+
+	// 1. Get the tx
+	tx, err := c.trpc.Tx(ctx, txHash, true)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. Get the block that contains the tx
+	blockRes, err := c.trpc.Block(context.Background(), &tx.Height)
+	if err != nil {
+		return nil, err
+	}
+
+	shareRange, err := square.TxShareRange(blockRes.Block.Data.Txs.ToSliceOfBytes(), int(tx.Index), blockRes.Block.Header.Version.App)
+	if err != nil {
+		return nil, err
+	}
+
+	h, err := c.client.Header.GetByHeight(ctx, uint64(tx.Height))
+	if err != nil {
+		return nil, err
+	}
+
+	shares, err := c.client.Share.GetSharesByNamespace(ctx, h, ns)
+	if err != nil {
+		return nil, err
+	}
+
+	return shares[shareRange.Start:shareRange.End], nil
 }

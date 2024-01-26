fe0656707858e22880533517df2b3866a258307d
KassCrypto <kasscrypto@gmail.com>
2024-01-26 14:38:20 +0000

    Refactor NewShareProof function to include attestations in the SharesProof struct

--- node/contracts/convert.go
+++ node/contracts/convert.go
@@ -1,58 +1,71 @@
 package contracts
 
 import (
-	"fmt"
 	chainloader "hummingbird/node/contracts/ChainLoader.sol"
+	"math/big"
 
 	"github.com/tendermint/tendermint/types"
 )
 
-func NewShareProof(proof *types.ShareProof) (*chainloader.SharesProof, error) {
-	return nil, fmt.Errorf("not implemented")
+func NewShareProof(proof *types.ShareProof, attestations chainloader.AttestationProof) (*chainloader.SharesProof, error) {
 
-	// sp := []chainloader.NamespaceMerkleMultiproof{}
-	// for _, s := range proof.ShareProofs {
+	// TODO: is this correct????
 
-	// 	sp = append(sp, chainloader.NamespaceMerkleMultiproof{
-	// 		BeginKey:  big.NewInt(int64(s.Start)),
-	// 		EndKey:    big.NewInt(int64(s.End)),
-	// 		SideNodes: []chainloader.NamespaceNode{},
-	// 	})
-	// }
+	ns := chainloader.Namespace{
+		Version: [1]byte(proof.NamespaceID),
+		Id:      [28]byte(proof.NamespaceID[:28]),
+	}
 
-	// ns := chainloader.Namespace{
-	// 	Version: [1]byte(proof.NamespaceID),
-	// 	Id:      [28]byte(proof.NamespaceID[:28]),
-	// }
+	sp := []chainloader.NamespaceMerkleMultiproof{}
+	for _, s := range proof.ShareProofs {
+		sideNodes := []chainloader.NamespaceNode{}
+		for _, sn := range s.Nodes {
+			sideNodes = append(sideNodes, chainloader.NamespaceNode{
+				Min:    ns,           // Not sure if this is correct???
+				Max:    ns,           // ?
+				Digest: [32]byte(sn), // ?
+			})
+		}
 
-	// rr := []chainloader.NamespaceNode{}
-	// for _, r := range proof.RowProof.RowRoots {
+		sp = append(sp, chainloader.NamespaceMerkleMultiproof{
+			BeginKey:  big.NewInt(int64(s.Start)),
+			EndKey:    big.NewInt(int64(s.End)),
+			SideNodes: sideNodes,
+		})
+	}
 
-	// 	rr = append(rr, chainloader.NamespaceNode{
-	// 		Min:    ns,
-	// 		Max:    ns,
-	// 		Digest: [32]byte(r.Bytes()),
-	// 	})
-	// }
+	rr := []chainloader.NamespaceNode{}
+	for _, r := range proof.RowProof.RowRoots {
 
-	// rp := []chainloader.BinaryMerkleProof{}
-	// for _, r := range proof.RowProof.Proofs {
+		rr = append(rr, chainloader.NamespaceNode{
+			Min:    ns,                  // Not sure if this is correct???
+			Max:    ns,                  // ?
+			Digest: [32]byte(r.Bytes()), // ?
+		})
 
-	// 	rp = append(rp, chainloader.BinaryMerkleProof{
-	// 		SideNodes: [][32]byte{},
-	// 		Key:       big.NewInt(r.Index),
-	// 		NumLeaves: big.NewInt(r.Total),
-	// 	})
-	// }
+	}
 
-	// return &chainloader.SharesProof{
-	// 	Data:             proof.Data,
-	// 	ShareProofs:      sp,
-	// 	Namespace:        ns,
-	// 	RowRoots:         rr,
-	// 	RowProofs:        []chainloader.BinaryMerkleProof{},
-	// 	AttestationProof: chainloader.AttestationProof{
+	rp := []chainloader.BinaryMerkleProof{}
+	for _, r := range proof.RowProof.Proofs {
 
-	// 	},
-	// }, nil
+		sideNodes := [][32]byte{}
+		for _, sn := range r.Aunts {
+			sideNodes = append(sideNodes, [32]byte(sn))
+		}
+
+		rp = append(rp, chainloader.BinaryMerkleProof{
+			SideNodes: sideNodes,
+			Key:       big.NewInt(r.Index),
+			NumLeaves: big.NewInt(r.Total),
+		})
+	}
+
+	return &chainloader.SharesProof{
+		Data:             proof.Data,
+		ShareProofs:      sp,
+		Namespace:        ns,
+		RowRoots:         rr,
+		RowProofs:        rp,
+		AttestationProof: attestations,
+	}, nil
 }

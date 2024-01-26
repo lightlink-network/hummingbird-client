f0a76b772f7c88d683dcb9f4af4abe0399cf3017
KassCrypto <kasscrypto@gmail.com>
2024-01-26 02:08:09 +0000

    Add FindHeaderShares method to Bundle

--- node/bundle.go
+++ node/bundle.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"bytes"
 	"fmt"
 	"hummingbird/utils"
 
@@ -110,4 +111,56 @@ func (b *Bundle) Blob(namespace string) (*blob.Blob, error) {
 
 	// 2. get the blob
 	return utils.BytesToBlob(namespace, bundleRLP)
+}
+
+// FinderHeaderShares finds the shares in the bundle which contain the header
+func (b *Bundle) FindHeaderShares(hash common.Hash, namespace string) (*SharePointer, error) {
+	// 1. find the block with the given hash
+	var block *types.Block
+	for _, b := range b.Blocks {
+		if b.Hash() == hash {
+			block = b
+			break
+		}
+	}
+	if block == nil {
+		return nil, fmt.Errorf("block with hash %s not found in bundle", hash.Hex())
+	}
+
+	// 2. get the header RLP
+	header := block.Header()
+	headerRLP, err := rlp.EncodeToBytes(header)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. Get the bundle shares
+	bundleRLP, err := b.EncodeRLP()
+	if err != nil {
+		return nil, err
+	}
+
+	// 4. get bundle shares
+	blob, err := utils.BytesToBlob(namespace, bundleRLP)
+	if err != nil {
+		return nil, err
+	}
+	shares, err := utils.BlobToShares(blob)
+	if err != nil {
+		return nil, err
+	}
+
+	// 5. find the header coords in the raw bundleRLP
+	rlpStart := bytes.Index(bundleRLP, headerRLP)
+	rlpEnd := rlpStart + len(headerRLP)
+
+	if rlpStart == -1 {
+		return nil, fmt.Errorf("encoded header not found in the bundle")
+	}
+
+	// 6. find the header coords in the shares
+	startShare, startIndex := utils.RawIndexToSharesIndex(rlpStart, shares)
+	endShare, endIndex := utils.RawIndexToSharesIndex(rlpEnd, shares)
+
+	return NewSharePointer(shares, startShare, startIndex, endShare, endIndex), nil
 }

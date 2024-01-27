02993ed7a1ca85aef5e1898599f8e959246fdda3
Sledro <dan@sledro.com>
2024-01-27 23:40:22 +0000

    add helper to convert challenge status enum to string

--- node/contracts/types.go
+++ node/contracts/types.go
@@ -9,3 +9,19 @@ type ChallengeDaInfo struct {
 	Expiry     *big.Int `pretty:"Expiry"`
 	Status     uint8    `pretty:"Status"`
 }
+
+// Helper to convert challenge status enum to string
+func StatusString(c uint8) string {
+	switch c {
+	case 0:
+		return "None"
+	case 1:
+		return "ChallengerInitiated"
+	case 2:
+		return "DefenderResponded"
+	case 3:
+		return "ChallengerWon"
+	default:
+		return "Unknown"
+	}
+}

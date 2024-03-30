70d4ffbc42d066892693c39f074caf94f92354a8
KassCrypto <kasscrypto@gmail.com>
2024-03-30 10:50:00 +0000

    new share conversion

--- node/contracts/convert.go
+++ node/contracts/convert.go
@@ -4,68 +4,130 @@ import (
 	chainoracle "hummingbird/node/contracts/ChainOracle.sol"
 	"math/big"
 
+	"github.com/tendermint/tendermint/crypto/merkle"
+	"github.com/tendermint/tendermint/libs/bytes"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"github.com/tendermint/tendermint/types"
 )
 
 func NewShareProof(proof *types.ShareProof, attestations chainoracle.AttestationProof) (*chainoracle.SharesProof, error) {
-
-	// TODO: is this correct????
-
-	ns := chainoracle.Namespace{
-		Version: [1]byte(proof.NamespaceID),
-		Id:      [28]byte(proof.NamespaceID[:28]),
-	}
-
-	sp := []chainoracle.NamespaceMerkleMultiproof{}
-	for _, s := range proof.ShareProofs {
-		sideNodes := []chainoracle.NamespaceNode{}
-		for _, sn := range s.Nodes {
-			sideNodes = append(sideNodes, chainoracle.NamespaceNode{
-				Min:    ns,           // Not sure if this is correct???
-				Max:    ns,           // ?
-				Digest: [32]byte(sn), // ?
-			})
-		}
-
-		sp = append(sp, chainoracle.NamespaceMerkleMultiproof{
-			BeginKey:  big.NewInt(int64(s.Start)),
-			EndKey:    big.NewInt(int64(s.End)),
-			SideNodes: sideNodes,
-		})
-	}
-
-	rr := []chainoracle.NamespaceNode{}
-	for _, r := range proof.RowProof.RowRoots {
-
-		rr = append(rr, chainoracle.NamespaceNode{
-			Min:    ns,                  // Not sure if this is correct???
-			Max:    ns,                  // ?
-			Digest: [32]byte(r.Bytes()), // ?
-		})
-
-	}
-
-	rp := []chainoracle.BinaryMerkleProof{}
-	for _, r := range proof.RowProof.Proofs {
-
-		sideNodes := [][32]byte{}
-		for _, sn := range r.Aunts {
-			sideNodes = append(sideNodes, [32]byte(sn))
-		}
-
-		rp = append(rp, chainoracle.BinaryMerkleProof{
-			SideNodes: sideNodes,
-			Key:       big.NewInt(r.Index),
-			NumLeaves: big.NewInt(r.Total),
-		})
-	}
-
 	return &chainoracle.SharesProof{
 		Data:             proof.Data,
-		ShareProofs:      sp,
-		Namespace:        ns,
-		RowRoots:         rr,
-		RowProofs:        rp,
+		ShareProofs:      toNamespaceMerkleMultiProofs(proof.ShareProofs),
+		Namespace:        *namespace(proof.NamespaceID),
+		RowRoots:         toRowRoots(proof.RowProof.RowRoots),
+		RowProofs:        toRowProofs(proof.RowProof.Proofs),
 		AttestationProof: attestations,
 	}, nil
+}
+
+func ToAttestationProof(
+	nonce uint64,
+	height uint64,
+	blockDataRoot [32]byte,
+	dataRootInclusionProof merkle.Proof,
+) chainoracle.AttestationProof {
+	sideNodes := make([][32]byte, len(dataRootInclusionProof.Aunts))
+	for i, sideNode := range dataRootInclusionProof.Aunts {
+		var bzSideNode [32]byte
+		copy(bzSideNode[:], sideNode)
+		sideNodes[i] = bzSideNode
+	}
+
+	return chainoracle.AttestationProof{
+		TupleRootNonce: big.NewInt(int64(nonce)),
+		Tuple: chainoracle.DataRootTuple{
+			Height:   big.NewInt(int64(height)),
+			DataRoot: blockDataRoot,
+		},
+		Proof: chainoracle.BinaryMerkleProof{
+			SideNodes: sideNodes,
+			Key:       big.NewInt(dataRootInclusionProof.Index),
+			NumLeaves: big.NewInt(dataRootInclusionProof.Total),
+		},
+	}
+}
+
+func toNamespaceMerkleMultiProofs(proofs []*tmproto.NMTProof) []chainoracle.NamespaceMerkleMultiproof {
+	shareProofs := make([]chainoracle.NamespaceMerkleMultiproof, len(proofs))
+	for i, proof := range proofs {
+		sideNodes := make([]chainoracle.NamespaceNode, len(proof.Nodes))
+		for j, node := range proof.Nodes {
+			sideNodes[j] = *toNamespaceNode(node)
+		}
+		shareProofs[i] = chainoracle.NamespaceMerkleMultiproof{
+			BeginKey:  big.NewInt(int64(proof.Start)),
+			EndKey:    big.NewInt(int64(proof.End)),
+			SideNodes: sideNodes,
+		}
+	}
+	return shareProofs
+}
+
+func namespace(namespaceID []byte) *chainoracle.Namespace {
+	version := namespaceID[0]
+	var id [28]byte
+	copy(id[:], namespaceID[1:])
+	return &chainoracle.Namespace{
+		Version: [1]byte{version},
+		Id:      id,
+	}
+}
+
+func minNamespace(innerNode []byte) *chainoracle.Namespace {
+	version := innerNode[0]
+	var id [28]byte
+	copy(id[:], innerNode[1:28])
+	return &chainoracle.Namespace{
+		Version: [1]byte{version},
+		Id:      id,
+	}
+}
+
+func maxNamespace(innerNode []byte) *chainoracle.Namespace {
+	version := innerNode[29]
+	var id [28]byte
+	copy(id[:], innerNode[30:57])
+	return &chainoracle.Namespace{
+		Version: [1]byte{version},
+		Id:      id,
+	}
+}
+
+func toNamespaceNode(node []byte) *chainoracle.NamespaceNode {
+	minNs := minNamespace(node)
+	maxNs := maxNamespace(node)
+	var digest [32]byte
+	copy(digest[:], node[58:])
+	return &chainoracle.NamespaceNode{
+		Min:    *minNs,
+		Max:    *maxNs,
+		Digest: digest,
+	}
+}
+
+func toRowRoots(roots []bytes.HexBytes) []chainoracle.NamespaceNode {
+	rowRoots := make([]chainoracle.NamespaceNode, len(roots))
+	for i, root := range roots {
+		rowRoots[i] = *toNamespaceNode(root.Bytes())
+	}
+	return rowRoots
+}
+
+func toRowProofs(proofs []*merkle.Proof) []chainoracle.BinaryMerkleProof {
+	rowProofs := make([]chainoracle.BinaryMerkleProof, len(proofs))
+	for i, proof := range proofs {
+		sideNodes := make([][32]byte, len(proof.Aunts))
+		for j, sideNode := range proof.Aunts {
+			var bzSideNode [32]byte
+			copy(bzSideNode[:], sideNode)
+			sideNodes[j] = bzSideNode
+		}
+		rowProofs[i] = chainoracle.BinaryMerkleProof{
+			SideNodes: sideNodes,
+			Key:       big.NewInt(proof.Index),
+			NumLeaves: big.NewInt(proof.Total),
+		}
+	}
+	return rowProofs
 }
